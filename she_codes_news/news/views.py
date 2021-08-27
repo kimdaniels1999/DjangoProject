b818b1b98298c1131e8f73d428511f445949dce2
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-08-28 06:52:38 +1000

    24.08.2021

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -26,3 +26,8 @@ class AddStoryView(generic.CreateView):
     context_object_name = 'storyForm'
     template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+        
